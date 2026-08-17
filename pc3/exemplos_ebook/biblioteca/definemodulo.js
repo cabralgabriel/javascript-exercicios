5cf9ef42e704b0b9d0361b67222a7d6999ced5ed
cabralgabriel <gabrielcabralv@hotmail.com>
2026-08-17 09:54:39 -0300

    refactor definemodulo

--- pc3/exemplos_ebook/biblioteca/definemodulo.js
+++ pc3/exemplos_ebook/biblioteca/definemodulo.js
@@ -2,13 +2,13 @@
 // Arquivo criado dentro de uma pasta na raiz do projeto
 
 function soma(x, y) {
-    let soma = x + y; // variável local
-    return soma;     // retorna o resultado
-  }
+  let soma = x + y; // variável local
+  return soma;     // retorna o resultado
+}
   
-  const subtrair = function(x, y) { 
-    return x - y; 
-  }
+const subtrair = function(x, y) { 
+  return x - y; 
+}
   
-  // módulos são utilizados para disponibilizar bibliotecas e funções e classes
-  module.exports = { soma, subtrair }
+// módulos são utilizados para disponibilizar bibliotecas e funções e classes
+module.exports = { soma, subtrair }
