ca07c9c1daebc431072881abe2357e98f839efd8
cabralgabriel <gabrielcabralv@hotmail.com>
2026-08-24 10:17:14 -0300

    adiciona classe conversor

---
+++ pc3/exercicios_fixacao/02_classe_conversores/main.js
@@ -0,0 +1,7 @@
+import { Conversor } from './Conversor.js';
+
+const conversor = new Conversor();
+
+console.log(conversor.celsiusParaFahrenheit(25));
+console.log(conversor.quilometrosParaMilhas(10));
+console.log(conversor.minutosParaHoras(150));
