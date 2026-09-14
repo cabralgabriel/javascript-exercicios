d34929617d99b93697b09900e8012c208d2215a1
Gabriel Cabral <gabrielcabralv@hotmail.com>
2026-09-14 10:24:35 -0300

    adiciona classe de Aluno

---
+++ pc3/exemplos_ebook/objetos/pessoas/Aluno.js
@@ -0,0 +1,50 @@
+// Arquivo criado em projeto no www.stackblitz.com denominado Aluno.js
+// Arquivo criado dentro de uma pasta /objetos/pessoas na raiz do projeto
+// Objetivo do exemplo: demonstrar conceitos de Herança, bem como
+// encapsulamento adicionados de condicionais utilizando operadores lógicos
+
+const PF = require('./PF');
+
+class Aluno extends PF {
+
+  #matricula;
+  #curso;
+
+  setMatricula(matricula) {
+    if (matricula) {
+      if ((matricula.length === 8) &&
+          (matricula.startsWith("20"))) {
+        this.#matricula = matricula;
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+
+  getMatricula() {
+    return this.#matricula;
+  }
+
+  setCurso(curso) {
+    if (curso) {
+      if ((curso === 'ADS') || (curso === 'SI')) {
+        this.#curso = curso;
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+
+  getCurso() {
+    return this.#curso;
+  }
+
+}
+
+module.exports = Aluno;
